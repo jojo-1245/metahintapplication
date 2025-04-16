0d7dfe4112b96998f0734d0219e6d86232c690a8
jojo-1245 <ekseh69@hanmail.net>
2025-04-16 16:55:20 +0900

    Create new functionality for Data synchronization

--- utils/fontSize.ts
+++
@@ -1,8 +0,0 @@
-export const fontSize = {
-  xs: 12,
-  sm: 14,
-  base: 16,
-  lg: 18,
-  xl: 20,
-  '2xl': 24,
-};
--- utils/maskEmail.ts
+++
@@ -1,9 +0,0 @@
-export const maskEmail = (email: string): string => {
-  const [localPart, domain] = email.split('@');
-  if (localPart.length <= 3) {
-    return `${localPart[0]}***@${domain}`;
-  }
-
-  const visiblePart = localPart.slice(0, 3);
-  return `${visiblePart}***@${domain}`;
-};
