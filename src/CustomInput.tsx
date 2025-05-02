2651917c5c321d23313965eadb4f260e6bfe89a0
jojo-1245 <ekseh69@hanmail.net>
2025-05-02 09:22:22 +0900

    Fix a bug in Registration flow

---
+++ src/CustomInput.tsx
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, StyleSheet, TextInputProps, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import i18n from '../../i18n';
+
+interface CustomInputProps extends TextInputProps {
+  label?: string;
+  type: 'name' | 'email' | 'phone' | 'password' | 'contract';
+  value: string;
+  onChangeText: (text: string) => void;
+  onValidationChange?: (isValid: boolean) => void;
+}
+
+const CustomInput: React.FC<CustomInputProps> = ({
+  label,
+  type,
+  value,
+  onChangeText,
+  onValidationChange,
+  ...rest
+}) => {
+  const [error, setError] = useState('');
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+
+  const validate = (text: string) => {
+    let valid = true;
+    let filtered = text;
+
+    if (type === 'name') {
+      // filtered = text.replace(/[^A-Za-z\s]/g, ''); // Remove all except character A-Z
+      // valid = /^[A-Za-z\s]*$/.test(text);
+      filtered = text.replace(/[\d\p{P}$+<=>^`|~]/gu, ''); // Just remove number & symbol but let all char (Latin, Hangeul, etc)
+      valid = text.trim().length > 0;
+
+      setError(valid ? '' : `*${i18n.t('customInput.name')}`);
+    }
+
+    if (type === 'email') {
+      valid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(text);
+      setError(valid || text === '' ? '' : `*${i18n.t('customInput.email')}`);
+    }
+
+    if (type === 'phone') {
+      filtered = text.replace(/[^0-9]/g, '');
+      valid = /^[0-9]*$/.test(text);
+      setError(valid ? '' : `*${i18n.t('customInput.phone')}`);
+    }
+
+    if (type === 'password') {
+      valid = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(text);
+      setError(valid || text === '' ? '' : `*${i18n.t('customInput.password')}`);
+    }
+
+    if (type === 'contract') {
+      valid = /^0x[a-fA-F0-9]{40}$/.test(text.trim());
+      setError(valid || text === '' ? '' : `*${i18n.t('customInput.contract')}`);
+    }
+
+    if (filtered !== text) {
+      onChangeText(filtered); // update dengan hasil filter
+    }
+
+    // Trigger ke parent
+    if (onValidationChange) {
+      onValidationChange(valid || text === '');
+    }
+  };
+
+  useEffect(() => {
+    validate(value);
+  }, [value]);
+
+  return (
+    <View style={styles.container}>
+      {label && <Text style={styles.label}>{label}</Text>}
+      <View style={styles.inputWrapper}>
+        <TextInput
+          key={type === 'password' && !isPasswordVisible ? 'hidden' : 'visible'}
+          style={styles.input}
+          value={value}
+          onChangeText={onChangeText}
+          secureTextEntry={type === 'password' && !isPasswordVisible}
+          keyboardType={
+            type === 'phone' ? 'number-pad' : type === 'email' ? 'email-address' : 'default'
+          }
+          autoCapitalize={type === 'email' || type === 'password' ? 'none' : 'words'}
+          {...rest}
+        />
+
+        {type === 'password' ? (
+          <TouchableOpacity
+            onPress={() => setPasswordVisible(!isPasswordVisible)}
+            style={styles.icon}
+          >
+            <Icon name={isPasswordVisible ? 'eye' : 'eye-off'} size={20} color="#666" />
+          </TouchableOpacity>
+        ) : value.length > 0 ? (
+          <TouchableOpacity onPress={() => onChangeText('')} style={styles.icon}>
+            <Icon name="close-circle" size={20} color="#666" />
+          </TouchableOpacity>
+        ) : null}
+      </View>
+
+      {!!error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
+  );
+};
+
+export default CustomInput;
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 12,
+  },
+  label: {
+    marginBottom: 6,
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#444',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FAFAFA',
+    borderRadius: 12,
+    paddingHorizontal: 12,
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 14,
+    fontSize: 16,
+    color: '#000',
+  },
+  icon: {
+    paddingLeft: 8,
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 9,
+    marginTop: 4,
+    marginLeft: 4,
+  },
+});
